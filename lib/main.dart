070a17ad1e2d68b45a89078fb4e1d2d95a7e3ee6
abdul awal <awal@twintechsoft.com>
2024-10-23 09:57:51 +0600

    random name picking is working with bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,90 @@
+import 'package:bloc/bloc.dart';
+import 'dart:math' as math show Random;
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ),);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const names = [
+  'Foo',
+  'Bar',
+  'Baz',
+];
 
-  // This widget is the root of your application.
+extension RandomElement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
+class NamesCubit extends Cubit<String?> {
+  NamesCubit() : super(null);
+
+  void pickRandomName() => emit(
+        names.getRandomElement(),
+      );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final NamesCubit cubit;
+
+  @override
+  void initState() {
+    super.initState();
+    cubit = NamesCubit();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    cubit.close();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
- 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: StreamBuilder(
+            stream: cubit.stream,
+            builder: (context, snapshot) {
+              final button = TextButton(
+                onPressed: () => cubit.pickRandomName(),
+                child: const Text(
+                  'Pick a Random Name',
+                ),
+              );
+
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return button;
+                case ConnectionState.waiting:
+                  return button;
+                case ConnectionState.active:
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(snapshot.data.toString()),
+                      button,
+                    ],
+                  );
+                case ConnectionState.done:
+                  return const SizedBox();
+              }
+            },
+          ),
+        ),
       ),
-      home: Scaffold(
-        body: Center(child: Text("Hello  world"),),
-      )
     );
   }
-} 
+}
